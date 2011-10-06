251739ed9c7fd1f0fc11e89418abbf31d3936de6
Samuel Rødal <samuel.rodal@nokia.com>
2011-10-06 10:24:05 +0200

    Fixed compile error (the old Status #define).
    Change-Id: I650085e7e95c8b561e2e114937576e032cffb383
    Reviewed-on: http://codereview.qt-project.org/6221
    Reviewed-by: Paul Olav Tvete <paul.tvete@nokia.com>
    Sanity-Review: Qt Sanity Bot <qt_sanity_bot@ovi.com>

--- src/qt-compositor/hardware_integration/wayland_egl/waylandeglintegration.cpp
+++ src/qt-compositor/hardware_integration/wayland_egl/waylandeglintegration.cpp
@@ -52,7 +52,6 @@
 #define GL_GLEXT_PROTOTYPES
 #include <GLES2/gl2.h>
 #include <GLES2/gl2ext.h>
-#include <QDebug>
 
 GraphicsHardwareIntegration * GraphicsHardwareIntegration::createGraphicsHardwareIntegration(WaylandCompositor *compositor)
 {

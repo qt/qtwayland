qt/qtwayland

commit c5163c53337b72e0c28aebe89580d2ef5986d339

Avoid flicker when switching back to composited mode
The surface item needs to create a new texture when the compositor
switches from direct render to composited mode.

Change-Id: Ic2d65929617f1941ea290645ac28b8e986c414bc
Reviewed-by: Samuel Rødal <samuel.rodal@nokia.com>

src/compositor/compositor_api/waylandsurfaceitem.cpp
@@ -248,6 +248,12 @@ void WaylandSurfaceItem::surfaceDestroyed(QObject *)
     m_surface = 0;
 }
 
+void WaylandSurfaceItem::setDamagedFlag(bool on)
+{
+    m_damaged = on;
+}
+
+
 void WaylandSurfaceItem::surfaceDamaged(const QRect &)
 {
     m_damaged = true;
@@ -298,13 +304,23 @@ void WaylandSurfaceItem::setPaintEnabled(bool enabled)
     update();
 }
 
-QSGNode *WaylandSurfaceItem::updatePaintNode(QSGNode *oldNode, UpdatePaintNodeData *)
+class WaylandSurfaceNode : public QSGSimpleTextureNode
 {
-    if (!m_surface) {
-        delete oldNode;
-        return 0;
+public:
+    WaylandSurfaceNode(WaylandSurfaceItem *item) : m_item(item) {
+        setFlag(UsePreprocess,true);
     }
+    void preprocess() {
+        if (m_item->m_damaged)
+            m_item->updateNodeTexture(this);
+    }
+private:
+    WaylandSurfaceItem *m_item;
+};
 
+
+void WaylandSurfaceItem::updateNodeTexture(WaylandSurfaceNode *node)
+{
     if (m_damaged) {
         QSGTexture *oldTexture = m_texture;
         if (m_surface->type() == WaylandSurface::Texture) {
@@ -327,24 +343,31 @@ QSGNode *WaylandSurfaceItem::updatePaintNode(QSGNode *oldNode, UpdatePaintNodeDa
         m_provider->smooth = smooth();
     }
 
-    QSGSimpleTextureNode *node = static_cast<QSGSimpleTextureNode *>(oldNode);
-    if (!m_texture || !m_paintEnabled) {
+    node->setTexture(m_texture);
+    node->setFiltering(smooth() ? QSGTexture::Linear : QSGTexture::Nearest);
+}
+
+
+QSGNode *WaylandSurfaceItem::updatePaintNode(QSGNode *oldNode, UpdatePaintNodeData *)
+{
+    if (!m_surface || !m_paintEnabled) {
         delete oldNode;
         return 0;
     }
 
+    WaylandSurfaceNode *node = static_cast<WaylandSurfaceNode *>(oldNode);
+
     if (!node) {
-        node = new QSGSimpleTextureNode();
+        node = new WaylandSurfaceNode(this);
     }
+
+    updateNodeTexture(node);
 
     if (surface()->isYInverted()) {
         node->setRect(0, height(), width(), -height());
     } else {
         node->setRect(0, 0, width(), height());
     }
-
-    node->setTexture(m_texture);
-    node->setFiltering(smooth() ? QSGTexture::Linear : QSGTexture::Nearest);
 
     return node;
 }

src/compositor/compositor_api/waylandsurfaceitem.h
@@ -50,6 +50,7 @@
 #include <QtQuick/qsgtextureprovider.h>
 
 class WaylandSurfaceTextureProvider;
+class WaylandSurfaceNode;
 
 Q_DECLARE_METATYPE(WaylandSurface*)
 
@@ -84,6 +85,8 @@ public:
     void setUseTextureAlpha(bool useTextureAlpha);
     void setClientRenderingEnabled(bool enabled);
     void setTouchEventsEnabled(bool enabled);
+
+    void setDamagedFlag(bool on);
 
 protected:
     void mousePressEvent(QMouseEvent *event);
@@ -120,6 +123,8 @@ protected:
     QSGNode *updatePaintNode(QSGNode *oldNode, UpdatePaintNodeData *);
 
 private:
+    friend class WaylandSurfaceNode;
+    void updateNodeTexture(WaylandSurfaceNode *newNode);
     QPoint toSurface(const QPointF &pos) const;
     void init(WaylandSurface *);
 

src/compositor/wayland_wrapper/wlsurface.cpp
@@ -41,6 +41,7 @@
 #include "wlsurface.h"
 
 #include "waylandsurface.h"
+#include "waylandsurfaceitem.h"
 
 #include "wlcompositor.h"
 #include "wlshmbuffer.h"
@@ -319,6 +320,9 @@ bool Surface::advanceBufferQueue()
 
 void Surface::doUpdate() {
     if (postBuffer()) {
+        WaylandSurfaceItem *surfaceItem = waylandSurface()->surfaceItem();
+        if (surfaceItem)
+            surfaceItem->setDamagedFlag(true); // avoid flicker when we switch back to composited mode
         sendFrameCallback();
     } else {
         SurfaceBuffer *surfaceBuffer = currentSurfaceBuffer();

commit ce1e3fd8dc416a9a4f6550a6c139aec435e79940

Fixed WaylandSurfaceItem for when paint enabled is false.
This broke in change c5163c53337b72e0c28ae. Even when paint enabled is
false meaning we don't create a paint node, we want to update the
QSGTexture and the texture provider, so that the WaylandSurfaceItem can
function as a shader effect source.

Change-Id: I8d40c774f8a28240902d2d9919aee8645db9dbe7
Reviewed-by: Paul Olav Tvete <paul.tvete@nokia.com>

src/compositor/compositor_api/waylandsurfaceitem.cpp
@@ -311,15 +311,17 @@ public:
         setFlag(UsePreprocess,true);
     }
     void preprocess() {
-        if (m_item->m_damaged)
+        if (m_item->m_damaged) {
+            m_item->updateTexture();
             m_item->updateNodeTexture(this);
+        }
     }
 private:
     WaylandSurfaceItem *m_item;
 };
 
 
-void WaylandSurfaceItem::updateNodeTexture(WaylandSurfaceNode *node)
+void WaylandSurfaceItem::updateTexture()
 {
     if (m_damaged) {
         QSGTexture *oldTexture = m_texture;
@@ -342,15 +344,23 @@ void WaylandSurfaceItem::updateNodeTexture(WaylandSurfaceNode *node)
         m_provider->t = m_texture;
         m_provider->smooth = smooth();
     }
+}
 
+void WaylandSurfaceItem::updateNodeTexture(WaylandSurfaceNode *node)
+{
     node->setTexture(m_texture);
     node->setFiltering(smooth() ? QSGTexture::Linear : QSGTexture::Nearest);
 }
 
-
 QSGNode *WaylandSurfaceItem::updatePaintNode(QSGNode *oldNode, UpdatePaintNodeData *)
 {
-    if (!m_surface || !m_paintEnabled) {
+    if (!m_surface) {
+        delete oldNode;
+        return 0;
+    }
+
+    updateTexture();
+    if (!m_texture || !m_paintEnabled) {
         delete oldNode;
         return 0;
     }

src/compositor/compositor_api/waylandsurfaceitem.h
@@ -124,6 +124,7 @@ protected:
 
 private:
     friend class WaylandSurfaceNode;
+    void updateTexture();
     void updateNodeTexture(WaylandSurfaceNode *newNode);
     QPoint toSurface(const QPointF &pos) const;
     void init(WaylandSurface *);
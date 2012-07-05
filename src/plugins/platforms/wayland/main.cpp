1d5dfb2d43fb96ec27e9b19d1c99de241c9e422c
Jorgen Lind <jorgen.lind@gmail.com>
2012-07-05 11:57:31 +0200

    Fix QPA plugin metadata change
    Change-Id: I4f56aba2d42f2f9b469bdcd8cf4312fee93ab911
    Reviewed-by: Paul Olav Tvete <paul.tvete@nokia.com>

--- src/plugins/platforms/wayland/main.cpp
+++ src/plugins/platforms/wayland/main.cpp
@@ -47,7 +47,7 @@ QT_BEGIN_NAMESPACE
 class QWaylandIntegrationPlugin : public QPlatformIntegrationPlugin
 {
     Q_OBJECT
-    Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QPlatformIntegrationFactoryInterface" FILE "wayland.json")
+    Q_PLUGIN_METADATA(IID "org.qt-project.Qt.QPA.QPlatformIntegrationFactoryInterface.5.1" FILE "wayland.json")
 public:
     QStringList keys() const;
     QPlatformIntegration *create(const QString&, const QStringList&);

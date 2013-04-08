ee70a5eb42f8a872060e9141ea6777dd8fb173f7
Jørgen Lind <jorgen.lind@digia.com>
2013-04-08 10:00:22 +0200

    Remove unnecessary blocking roundtrip
    Change-Id: Ie12cdc1d2d931dc86497188d69b34bae47fa6dcc
    Reviewed-by: Samuel Rødal <samuel.rodal@digia.com>
    Reviewed-by: Andy Nichols <andy.nichols@digia.com>

--- src/plugins/platforms/qwayland-egl/qwaylandeglwindow.cpp
+++ src/plugins/platforms/qwayland-egl/qwaylandeglwindow.cpp
@@ -98,7 +98,6 @@ void QWaylandEglWindow::setGeometry(const QRect &rect)
         int current_width, current_height;
         wl_egl_window_get_attached_size(m_waylandEglWindow,&current_width,&current_height);
         if (current_width != sizeWithMargins.width() || current_height != sizeWithMargins.height()) {
-            waitForFrameSync();
             wl_egl_window_resize(m_waylandEglWindow, sizeWithMargins.width(), sizeWithMargins.height(), mOffset.x(), mOffset.y());
             mOffset = QPoint();
 

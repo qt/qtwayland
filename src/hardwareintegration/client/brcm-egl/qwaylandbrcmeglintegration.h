aad4549584498dddf7b08df595417b4ad0a05419
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2015-03-04 19:28:15 +0000

    brcm-egl: Disambiguate wl_registry
    Include wayland-client.h to disambiguate between QtWaylandClient::wl_registry
    and wl_registry.
    
    Change-Id: Idf33fd56a6b2c2436f5cbabc953980d05826ebba
    Reviewed-by: Olivier Blin <qt@blino.org>
    Reviewed-by: Giulio Camuffo <giulio.camuffo@jollamobile.com>

--- src/hardwareintegration/client/brcm-egl/qwaylandbrcmeglintegration.h
+++ src/hardwareintegration/client/brcm-egl/qwaylandbrcmeglintegration.h
@@ -43,6 +43,7 @@
 #define QWAYLANDBRCMEGLINTEGRATION_H
 
 #include <QtWaylandClient/private/qwaylandclientbufferintegration_p.h>
+#include <wayland-client.h>
 
 #include <EGL/egl.h>
 #include <EGL/eglext.h>

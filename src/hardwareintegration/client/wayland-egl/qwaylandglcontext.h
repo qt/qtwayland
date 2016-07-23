da3bd879576eab8230f73dbfc4d4fad2bb5636a6
Thiago Macieira <thiago.macieira@intel.com>
2016-07-22 20:33:19 -0700

    Fix build with ICC 17: problem parsing override keyword
    So just use the typedef that QPlatformOpenGLContext defines for a
    function pointer.
    
    qwaylandglcontext.h(76): error: expected a ";"
    
    Intel Issue ID: 6000161944
    Change-Id: I149e0540c00745fe8119fffd1463ce5c4ffdeb4c
    Reviewed-by: Jan Arne Petersen <jan.petersen@kdab.com>
    Reviewed-by: Johan Helsing <johan.helsing@qt.io>
    Reviewed-by: Giulio Camuffo <giulio.camuffo@kdab.com>

--- src/hardwareintegration/client/wayland-egl/qwaylandglcontext.h
+++ src/hardwareintegration/client/wayland-egl/qwaylandglcontext.h
@@ -73,7 +73,7 @@ public:
     bool isSharing() const Q_DECL_OVERRIDE;
     bool isValid() const Q_DECL_OVERRIDE;
 
-    void (*getProcAddress(const char *procName)) () Q_DECL_OVERRIDE;
+    QFunctionPointer getProcAddress(const char *procName) Q_DECL_OVERRIDE;
 
     QSurfaceFormat format() const Q_DECL_OVERRIDE { return m_format; }
 

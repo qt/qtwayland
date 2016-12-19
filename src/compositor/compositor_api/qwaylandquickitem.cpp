qt/qtwayland

commit 4e7af898b28fa8995582a4c0df0455a575525911

Add support for running compositors with custom device pixel ratio
And add a manual test for showing that it works with different scale factors as
well.

Task-number: QTBUG-55303
Change-Id: Ib842ea51cfb62a2d46713e95d56b8a9da445d14d
Reviewed-by: Paul Olav Tvete <paul.tvete@qt.io>
Reviewed-by: Pier Luigi Fiorini <pierluigi.fiorini@hawaiios.org>

src/compositor/compositor_api/qwaylandquickitem.cpp
@@ -1266,5 +1266,8 @@ void QWaylandQuickItem::handleDragStarted(QWaylandDrag *drag)
     d->isDragging = true;
 }
 
-QT_END_NAMESPACE
+qreal QWaylandQuickItemPrivate::scaleFactor() const {
+    return (view->output() ? view->output()->scaleFactor() : 1) / window->devicePixelRatio();
+}
 
+QT_END_NAMESPACE

src/compositor/compositor_api/qwaylandoutput.cpp
@@ -276,7 +276,7 @@ void QWaylandOutput::initialize()
     // Replace modes with one that follows the window size and refresh rate,
     // but only if window size is valid
     if (d->window && d->sizeFollowsWindow) {
-        QWaylandOutputMode mode(d->window->size(),
+        QWaylandOutputMode mode(d->window->size() * d->window->devicePixelRatio(),
                                 qFloor(d->window->screen()->refreshRate() * 1000));
         if (mode.isValid()) {
             d->modes.clear();
@@ -933,13 +933,13 @@ void QWaylandOutput::handleSetWidth(int newWidth)
     if (d->currentMode <= d->modes.size() - 1) {
         if (d->currentMode >= 0) {
             QWaylandOutputMode mode = d->modes.at(d->currentMode);
-            mode.setWidth(newWidth);
+            mode.setWidth(newWidth * d->window->devicePixelRatio());
             d->modes.replace(d->currentMode, mode);
             d->sendModesInfo();
         } else {
             // We didn't add a mode during the initialization because the window
             // size was invalid, let's add it now
-            QWaylandOutputMode mode(d->window->size(),
+            QWaylandOutputMode mode(d->window->size() * d->window->devicePixelRatio(),
                                     qFloor(d->window->screen()->refreshRate() * 1000));
             if (mode.isValid()) {
                 d->modes.clear();
@@ -963,13 +963,13 @@ void QWaylandOutput::handleSetHeight(int newHeight)
     if (d->currentMode <= d->modes.size() - 1) {
         if (d->currentMode >= 0) {
             QWaylandOutputMode mode = d->modes.at(d->currentMode);
-            mode.setHeight(newHeight);
+            mode.setHeight(newHeight * d->window->devicePixelRatio());
             d->modes.replace(d->currentMode, mode);
             d->sendModesInfo();
         } else {
             // We didn't add a mode during the initialization because the window
             // size was invalid, let's add it now
-            QWaylandOutputMode mode(d->window->size(),
+            QWaylandOutputMode mode(d->window->size() * d->window->devicePixelRatio(),
                                     qFloor(d->window->screen()->refreshRate() * 1000));
             if (mode.isValid()) {
                 d->modes.clear();

src/compositor/compositor_api/qwaylandquickitem_p.h
@@ -159,7 +159,7 @@ public:
     }
 
     bool shouldSendInputEvents() const { return view->surface() && inputEventsEnabled; }
-    int scaleFactor() const { return view->output() ? view->output()->scaleFactor() : 1; }
+    qreal scaleFactor() const;
 
     static QMutex *mutex;
 

src/compositor/extensions/qwaylandwlshellintegration.cpp
@@ -125,7 +125,8 @@ void WlShellIntegration::handleSetMaximized(QWaylandOutput *output)
     nextState = State::Maximized;
     finalPosition = designatedOutput->position() + designatedOutput->availableGeometry().topLeft();
 
-    m_shellSurface->sendConfigure(designatedOutput->availableGeometry().size(), QWaylandWlShellSurface::NoneEdge);
+    auto scaleFactor = m_item->view()->output()->scaleFactor();
+    m_shellSurface->sendConfigure(designatedOutput->availableGeometry().size() / scaleFactor, QWaylandWlShellSurface::NoneEdge);
 }
 
 void WlShellIntegration::handleSetFullScreen(QWaylandWlShellSurface::FullScreenMethod method, uint framerate, QWaylandOutput *output)
@@ -176,8 +177,9 @@ void WlShellIntegration::handleSetPopup(QWaylandSeat *seat, QWaylandSurface *par
         t.clear(&t);
         m_item->setRotation(0);
         m_item->setScale(1.0);
-        m_item->setX(relativeToParent.x());
-        m_item->setY(relativeToParent.y());
+        auto scaleFactor = m_item->output()->scaleFactor() / devicePixelRatio();
+        m_item->setX(relativeToParent.x() * scaleFactor);
+        m_item->setY(relativeToParent.y() * scaleFactor);
         m_item->setParentItem(parentItem);
     }
 
@@ -204,6 +206,11 @@ void WlShellIntegration::handlePopupRemoved()
     if (!m_shellSurface || m_shellSurface->shell()->mappedPopups().isEmpty())
         QWaylandQuickShellEventFilter::cancelFilter();
     isPopup = false;
+}
+
+qreal WlShellIntegration::devicePixelRatio() const
+{
+    return m_item->window() ? m_item->window()->devicePixelRatio() : 1;
 }
 
 void WlShellIntegration::handleShellSurfaceDestroyed()
@@ -237,7 +244,7 @@ void WlShellIntegration::adjustOffsetForNextFrame(const QPointF &offset)
 
     float scaleFactor = m_item->view()->output()->scaleFactor();
     QQuickItem *moveItem = m_item->moveItem();
-    moveItem->setPosition(moveItem->position() + offset * scaleFactor);
+    moveItem->setPosition(moveItem->position() + offset * scaleFactor / devicePixelRatio());
 }
 
 bool WlShellIntegration::mouseMoveEvent(QMouseEvent *event)
@@ -250,7 +257,7 @@ bool WlShellIntegration::mouseMoveEvent(QMouseEvent *event)
             return true;
         }
         float scaleFactor = m_item->view()->output()->scaleFactor();
-        QPointF delta = (event->windowPos() - resizeState.initialMousePos) / scaleFactor;
+        QPointF delta = (event->windowPos() - resizeState.initialMousePos) / scaleFactor * devicePixelRatio();
         QSize newSize = m_shellSurface->sizeForResize(resizeState.initialSize, delta, resizeState.resizeEdges);
         m_shellSurface->sendConfigure(newSize, resizeState.resizeEdges);
     } else if (grabberState == GrabberState::Move) {

src/compositor/extensions/qwaylandwlshellintegration_p.h
@@ -86,6 +86,7 @@ private:
 
     void handlePopupClosed();
     void handlePopupRemoved();
+    qreal devicePixelRatio() const;
 
     QWaylandQuickShellSurfaceItem *m_item;
     QPointer<QWaylandWlShellSurface> m_shellSurface;

tests/manual/scaling-compositor/main.cpp
@@ -1,0 +1,49 @@
+/****************************************************************************
+**
+** Copyright (C) 2016 The Qt Company Ltd.
+** Contact: http://www.qt-project.org/legal
+**
+** This file is part of the examples of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:BSD$
+** You may use this file under the terms of the BSD license as follows:
+**
+** "Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions are
+** met:
+**   * Redistributions of source code must retain the above copyright
+**     notice, this list of conditions and the following disclaimer.
+**   * Redistributions in binary form must reproduce the above copyright
+**     notice, this list of conditions and the following disclaimer in
+**     the documentation and/or other materials provided with the
+**     distribution.
+**   * Neither the name of The Qt Company Ltd nor the names of its
+**     contributors may be used to endorse or promote products derived
+**     from this software without specific prior written permission.
+**
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#include <QtGui/QGuiApplication>
+#include <QtQml/QQmlApplicationEngine>
+
+int main(int argc, char* argv[])
+{
+    QGuiApplication app(argc, argv);
+    QQmlApplicationEngine engine(QUrl("qrc:/main.qml"));
+    return app.exec();
+}

tests/manual/scaling-compositor/main.qml
@@ -1,0 +1,84 @@
+/****************************************************************************
+**
+** Copyright (C) 2016 The Qt Company Ltd.
+** Contact: http://www.qt-project.org/legal
+**
+** This file is part of the examples of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:BSD$
+** You may use this file under the terms of the BSD license as follows:
+**
+** "Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions are
+** met:
+**   * Redistributions of source code must retain the above copyright
+**     notice, this list of conditions and the following disclaimer.
+**   * Redistributions in binary form must reproduce the above copyright
+**     notice, this list of conditions and the following disclaimer in
+**     the documentation and/or other materials provided with the
+**     distribution.
+**   * Neither the name of The Qt Company Ltd nor the names of its
+**     contributors may be used to endorse or promote products derived
+**     from this software without specific prior written permission.
+**
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+import QtQuick 2.2
+import QtQuick.Window 2.2
+import QtQuick.Controls 2.0
+import QtWayland.Compositor 1.0
+
+WaylandCompositor {
+    id: comp
+    WaylandOutput {
+        id: output
+        compositor: comp
+        sizeFollowsWindow: true
+        scaleFactor: 2
+        window: Window {
+            id: win
+            width: 500
+            height: 500
+            visible: true
+            title: "Scaling compositor x" + output.scaleFactor
+            Button {
+                id: incrementButton
+                text: "+"
+                onClicked: ++output.scaleFactor
+            }
+            Button {
+                text: "-"
+                onClicked: output.scaleFactor = Math.max(1, output.scaleFactor - 1)
+                anchors.left: incrementButton.right
+            }
+            Repeater {
+                model: shellSurfaces
+                ShellSurfaceItem {
+                    shellSurface: modelData
+                    onSurfaceDestroyed: shellSurfaces.remove(index);
+                }
+            }
+        }
+    }
+
+    ListModel { id: shellSurfaces }
+
+    WlShell {
+        onWlShellSurfaceCreated: shellSurfaces.append({shellSurface: shellSurface});
+    }
+}

tests/manual/scaling-compositor/qml.qrc
@@ -1,0 +1,5 @@
+<!DOCTYPE RCC><RCC version="1.0">
+<qresource>
+    <file>main.qml</file>
+</qresource>
+</RCC>

commit c9ba2178388b6a1b68eae69709d989d6d4f19d9b

Fix nullptr crash introduced in 4e7af89
This occurred with any system-ui using the qtapplicationmanager when
trying to show the first window of an application.

Change-Id: Ifec4626269f30f91cd0f4e29871d33747d9b2374
Reviewed-by: Johan Helsing <johan.helsing@qt.io>
Reviewed-by: Jan Arne Petersen <jan.petersen@kdab.com>

src/compositor/compositor_api/qwaylandquickitem.cpp
@@ -1266,8 +1266,10 @@ void QWaylandQuickItem::handleDragStarted(QWaylandDrag *drag)
     d->isDragging = true;
 }
 
-qreal QWaylandQuickItemPrivate::scaleFactor() const {
-    return (view->output() ? view->output()->scaleFactor() : 1) / window->devicePixelRatio();
+qreal QWaylandQuickItemPrivate::scaleFactor() const
+{
+    return (view->output() ? view->output()->scaleFactor() : 1)
+            / (window ? window->devicePixelRatio() : 1);
 }
 
 QT_END_NAMESPACE
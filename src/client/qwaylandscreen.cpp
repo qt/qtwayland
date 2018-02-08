13aaed5806c3a5ef90a11757e7aeb92360c71605
Johan Klokkhammer Helsing <johan.helsing@qt.io>
2018-02-08 16:53:39 +0100

    Fix crash when connecting a new screen
    In QWaylandWindow::virtualSiblings, don't include screens that have not been
    added yet. I.e. QWaylandScreens for which QPlatformIntegration::screenAdded has
    not yet been called.
    
    The cherry-picked version does not include the tests from the original patch.
    
    Task-number: QTBUG-62044
    Change-Id: I623fbf8799d21c6b9293e7120ded301277639cc6
    Reviewed-by: David Edmundson <davidedmundson@kde.org>
    Reviewed-by: Aleix Pol
    Reviewed-by: Paul Olav Tvete <paul.tvete@qt.io>
    (cherry picked from commit fd9fec4fc7f43fb939e8e5a946c7858390bbd9d3)

--- src/client/qwaylandscreen.cpp
+++ src/client/qwaylandscreen.cpp
@@ -128,8 +128,10 @@ QList<QPlatformScreen *> QWaylandScreen::virtualSiblings() const
     QList<QPlatformScreen *> list;
     const QList<QWaylandScreen*> screens = mWaylandDisplay->screens();
     list.reserve(screens.count());
-    foreach (QWaylandScreen *screen, screens)
-        list << screen;
+    for (QWaylandScreen *screen : qAsConst(screens)) {
+        if (screen->screen())
+            list << screen;
+    }
     return list;
 }
 

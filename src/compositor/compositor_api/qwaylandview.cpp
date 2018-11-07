qt/qtwayland

commit 297bcd005c4205f69e1bf9e9dc565ab757cf0bac

Don't destroy buffer before surfaceDestroyed signal
Give the application time to set buffer lock first.

Change-Id: Id8a7ee8956bc587344b8eece7072edff5197414c
Task-number: QTBUG-69384
Reviewed-by: Johan Helsing <johan.helsing@qt.io>

src/compositor/compositor_api/qwaylandview.cpp
@@ -55,8 +55,9 @@ void QWaylandViewPrivate::markSurfaceAsDestroyed(QWaylandSurface *surface)
     Q_Q(QWaylandView);
     Q_ASSERT(surface == this->surface);
 
-    q->setSurface(nullptr);
+    setSurface(nullptr);
     emit q->surfaceDestroyed();
+    clearFrontBuffer();
 }
 
 /*!
@@ -132,38 +133,46 @@ QWaylandSurface *QWaylandView::surface() const
     return d->surface;
 }
 
+
+void QWaylandViewPrivate::setSurface(QWaylandSurface *newSurface)
+{
+    Q_Q(QWaylandView);
+    if (surface) {
+        QWaylandSurfacePrivate::get(surface)->derefView(q);
+        if (output)
+            QWaylandOutputPrivate::get(output)->removeView(q, surface);
+    }
+
+    surface = newSurface;
+
+    nextBuffer = QWaylandBufferRef();
+    nextBufferCommitted = false;
+    nextDamage = QRegion();
+
+    if (surface) {
+        QWaylandSurfacePrivate::get(surface)->refView(q);
+        if (output)
+            QWaylandOutputPrivate::get(output)->addView(q, surface);
+    }
+}
+
+void QWaylandViewPrivate::clearFrontBuffer()
+{
+    if (!bufferLocked) {
+        currentBuffer = QWaylandBufferRef();
+        currentDamage = QRegion();
+    }
+}
+
 void QWaylandView::setSurface(QWaylandSurface *newSurface)
 {
     Q_D(QWaylandView);
     if (d->surface == newSurface)
         return;
 
-
-    if (d->surface) {
-        QWaylandSurfacePrivate::get(d->surface)->derefView(this);
-        if (d->output)
-            QWaylandOutputPrivate::get(d->output)->removeView(this, d->surface);
-    }
-
-    d->surface = newSurface;
-
-    if (!d->bufferLocked) {
-        d->currentBuffer = QWaylandBufferRef();
-        d->currentDamage = QRegion();
-    }
-
-    d->nextBuffer = QWaylandBufferRef();
-    d->nextBufferCommitted = false;
-    d->nextDamage = QRegion();
-
-    if (d->surface) {
-        QWaylandSurfacePrivate::get(d->surface)->refView(this);
-        if (d->output)
-            QWaylandOutputPrivate::get(d->output)->addView(this, d->surface);
-    }
-
+    d->setSurface(newSurface);
+    d->clearFrontBuffer();
     emit surfaceChanged();
-
 }
 
 /*!

src/compositor/compositor_api/qwaylandview_p.h
@@ -74,6 +74,8 @@ public:
     { }
 
     void markSurfaceAsDestroyed(QWaylandSurface *surface);
+    void setSurface(QWaylandSurface *newSurface);
+    void clearFrontBuffer();
 
     QObject *renderObject = nullptr;
     QWaylandSurface *surface = nullptr;

commit f890798053c7fa692bff7f8a3e96583dfcaba0cb

Don't crash if view is deleted from surfaceDestroyed signal
Fix regression introduced in 297bcd005c4205f69e1bf9e9dc565ab757cf0bac

Fixes: QTBUG-71643
Change-Id: I769518a9ca13fcd13ea277c7fa8ac1885ca123ba
Reviewed-by: Johan Helsing <johan.helsing@qt.io>

src/compositor/compositor_api/qwaylandview.cpp
@@ -56,8 +56,10 @@ void QWaylandViewPrivate::markSurfaceAsDestroyed(QWaylandSurface *surface)
     Q_ASSERT(surface == this->surface);
 
     setSurface(nullptr);
+    QPointer<QWaylandView> deleteGuard(q);
     emit q->surfaceDestroyed();
-    clearFrontBuffer();
+    if (!deleteGuard.isNull())
+        clearFrontBuffer();
 }
 
 /*!
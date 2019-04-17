qt/qtwayland

commit d65783c9e1f40e176943b8706bf6bbed8320bcc8

compositor: fix QWaylandShell private objects inheritance
If the public API has inheritance relationships, this should be
reflected in the private classes too.
Otherwise the d_func will cast to a wrong type, leading to crashes.

Change-Id: Iad2dccad4b63326e6b108a44c708ab51f0784678
Reviewed-by: Johan Helsing <johan.helsing@qt.io>
Reviewed-by: Paul Olav Tvete <paul.tvete@qt.io>

src/compositor/extensions/qwaylandshell.h
@@ -96,11 +96,11 @@ public:
     }
 
 protected:
-    QWaylandShellTemplate(QWaylandCompositorExtensionPrivate &dd)
+    QWaylandShellTemplate(QWaylandShellPrivate &dd)
         : QWaylandShell(dd)
     { }
 
-    QWaylandShellTemplate(QWaylandObject *container, QWaylandCompositorExtensionPrivate &dd)
+    QWaylandShellTemplate(QWaylandObject *container, QWaylandShellPrivate &dd)
         : QWaylandShell(container,dd)
     { }
 };

src/compositor/extensions/qwaylandwlshell_p.h
@@ -43,6 +43,7 @@
 #include <QtWaylandCompositor/qtwaylandcompositorglobal.h>
 #include <QtWaylandCompositor/qwaylandsurface.h>
 #include <QtWaylandCompositor/private/qwaylandcompositorextension_p.h>
+#include <QtWaylandCompositor/private/qwaylandshell_p.h>
 #include <QtWaylandCompositor/QWaylandWlShellSurface>
 #include <QtWaylandCompositor/QWaylandSeat>
 
@@ -67,7 +68,7 @@
 QT_BEGIN_NAMESPACE
 
 class Q_WAYLAND_COMPOSITOR_EXPORT QWaylandWlShellPrivate
-                                        : public QWaylandCompositorExtensionPrivate
+                                        : public QWaylandShellPrivate
                                         , public QtWaylandServer::wl_shell
 {
     Q_DECLARE_PUBLIC(QWaylandWlShell)

src/compositor/extensions/qwaylandxdgshell_p.h
@@ -38,6 +38,7 @@
 #define QWAYLANDXDGSHELL_P_H
 
 #include <QtWaylandCompositor/private/qwaylandcompositorextension_p.h>
+#include <QtWaylandCompositor/private/qwaylandshell_p.h>
 #include <QtWaylandCompositor/private/qwayland-server-xdg-shell.h>
 
 #include <QtWaylandCompositor/QWaylandXdgShell>
@@ -73,7 +74,7 @@ struct Q_WAYLAND_COMPOSITOR_EXPORT QWaylandXdgPositionerData {
 };
 
 class Q_WAYLAND_COMPOSITOR_EXPORT QWaylandXdgShellPrivate
-        : public QWaylandCompositorExtensionPrivate
+        : public QWaylandShellPrivate
         , public QtWaylandServer::xdg_wm_base
 {
     Q_DECLARE_PUBLIC(QWaylandXdgShell)

src/compositor/extensions/qwaylandxdgshellv5_p.h
@@ -41,6 +41,7 @@
 #define QWAYLANDXDGSHELLV5_P_H
 
 #include <QtWaylandCompositor/private/qwaylandcompositorextension_p.h>
+#include <QtWaylandCompositor/private/qwaylandshell_p.h>
 #include <QtWaylandCompositor/private/qwayland-server-xdg-shell-unstable-v5_p.h>
 
 #include <QtWaylandCompositor/QWaylandXdgShellV5>
@@ -61,7 +62,7 @@
 QT_BEGIN_NAMESPACE
 
 class Q_WAYLAND_COMPOSITOR_EXPORT QWaylandXdgShellV5Private
-        : public QWaylandCompositorExtensionPrivate
+        : public QWaylandShellPrivate
         , public QtWaylandServer::xdg_shell_v5
 {
     Q_DECLARE_PUBLIC(QWaylandXdgShellV5)

src/compositor/extensions/qwaylandxdgshellv6_p.h
@@ -38,6 +38,7 @@
 #define QWAYLANDXDGSHELLV6_P_H
 
 #include <QtWaylandCompositor/private/qwaylandcompositorextension_p.h>
+#include <QtWaylandCompositor/private/qwaylandshell_p.h>
 #include <QtWaylandCompositor/private/qwayland-server-xdg-shell-unstable-v6.h>
 
 #include <QtWaylandCompositor/QWaylandXdgShellV6>
@@ -71,7 +72,7 @@ struct Q_WAYLAND_COMPOSITOR_EXPORT QWaylandXdgPositionerV6Data {
 };
 
 class Q_WAYLAND_COMPOSITOR_EXPORT QWaylandXdgShellV6Private
-        : public QWaylandCompositorExtensionPrivate
+        : public QWaylandShellPrivate
         , public QtWaylandServer::zxdg_shell_v6
 {
     Q_DECLARE_PUBLIC(QWaylandXdgShellV6)

commit c53bcc3b2ec04db86f77e9b3d0d18cef2c6932a0

Compositor: Fix compilation problems for qwaylandshell.h
d65783c broke compilation for code that includes qwaylandshell.h, but not
qwaylandshell_p.h, because QWaylandShellPrivate is an incomplete type and we
don't know that it's convertible to QWaylandCompositorExtensionPrivate by just
looking at the public header. And we can't move the QWaylandShellTemplate
constructors to the cpp file because it's a template class.

Change-Id: Ia2d093de74429ace33d08b91743cbc8b7e7befa8
Reviewed-by: Paul Olav Tvete <paul.tvete@qt.io>

src/compositor/extensions/qwaylandshell.cpp
@@ -92,4 +92,24 @@ void QWaylandShell::setFocusPolicy(QWaylandShell::FocusPolicy focusPolicy)
     emit focusPolicyChanged();
 }
 
+QWaylandShell::QWaylandShell(QWaylandShellPrivate &dd)
+    : QWaylandCompositorExtension(dd)
+{
+}
+
+QWaylandShell::QWaylandShell(QWaylandObject *container, QWaylandShellPrivate &dd)
+    : QWaylandCompositorExtension(container, dd)
+{
+}
+
+QWaylandShell::QWaylandShell(QWaylandCompositorExtensionPrivate &dd)
+    : QWaylandShell(static_cast<QWaylandShellPrivate &>(dd))
+{
+}
+
+QWaylandShell::QWaylandShell(QWaylandObject *container, QWaylandCompositorExtensionPrivate &dd)
+    : QWaylandShell(container, static_cast<QWaylandShellPrivate &>(dd))
+{
+}
+
 QT_END_NAMESPACE

src/compositor/extensions/qwaylandshell.h
@@ -68,8 +68,12 @@ Q_SIGNALS:
     void focusPolicyChanged();
 
 protected:
-    QWaylandShell(QWaylandCompositorExtensionPrivate &dd) : QWaylandCompositorExtension(dd) {}
-    QWaylandShell(QWaylandObject *container, QWaylandCompositorExtensionPrivate &dd) : QWaylandCompositorExtension(container, dd) {}
+    explicit QWaylandShell(QWaylandShellPrivate &dd);
+    explicit QWaylandShell(QWaylandObject *container, QWaylandShellPrivate &dd);
+
+    //Qt 6: remove
+    Q_DECL_DEPRECATED QWaylandShell(QWaylandCompositorExtensionPrivate &dd);
+    Q_DECL_DEPRECATED QWaylandShell(QWaylandObject *container, QWaylandCompositorExtensionPrivate &dd);
 };
 
 template <typename T>
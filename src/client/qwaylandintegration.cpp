qt/qtwayland

commit 0761173a046f8f10cc185cff9c524ae6a5284556

Load the Linux SPI Accessibility bridge
This is mostly a copy of XCB's atspi brige loading, only it creates the
original QPlatformAccessility() instance if the atspi bridge is not
available, to match the current behavior.

This also moves accesibility module to be lazy-loaded to match both Mir
and XCB QPAs.

Change-Id: I7b7ccadbec3760ab8a9449124db0db3f28c1f355
Reviewed-by: Frederik Gladhorn <frederik.gladhorn@qt.io>

src/client/qwaylandintegration.cpp
@@ -86,6 +86,10 @@
 #include "qwaylandinputdeviceintegration_p.h"
 #include "qwaylandinputdeviceintegrationfactory_p.h"
 
+#ifndef QT_NO_ACCESSIBILITY_ATSPI_BRIDGE
+#include <QtLinuxAccessibilitySupport/private/bridge_p.h>
+#endif
+
 QT_BEGIN_NAMESPACE
 
 namespace QtWaylandClient {
@@ -129,9 +133,6 @@ QWaylandIntegration::QWaylandIntegration()
     : mFontDb(new QGenericUnixFontDatabase())
 #endif
     , mNativeInterface(new QWaylandNativeInterface(this))
-#if QT_CONFIG(accessibility)
-    , mAccessibility(new QPlatformAccessibility())
-#endif
 {
     initializeInputDeviceIntegration();
     mDisplay.reset(new QWaylandDisplay(this));
@@ -277,6 +278,15 @@ QVariant QWaylandIntegration::styleHint(StyleHint hint) const
 #if QT_CONFIG(accessibility)
 QPlatformAccessibility *QWaylandIntegration::accessibility() const
 {
+    if (!mAccessibility) {
+#ifndef QT_NO_ACCESSIBILITY_ATSPI_BRIDGE
+        Q_ASSERT_X(QCoreApplication::eventDispatcher(), "QXcbIntegration",
+            "Initializing accessibility without event-dispatcher!");
+        mAccessibility.reset(new QSpiAccessibleBridge());
+#else
+        mAccessibility.reset(new QPlatformAccessibility());
+#endif
+    }
     return mAccessibility.data();
 }
 #endif

src/client/client.pro
@@ -18,6 +18,9 @@ CONFIG += link_pkgconfig wayland-scanner
 qtConfig(xkbcommon-evdev): \
     QMAKE_USE_PRIVATE += xkbcommon_evdev
 
+qtHaveModule(linuxaccessibility_support_private): \
+    QT += linuxaccessibility_support_private
+
 QMAKE_USE += wayland-client
 
 INCLUDEPATH += $$PWD/../shared

src/client/qwaylandintegration_p.h
@@ -145,7 +145,7 @@ private:
     QScopedPointer<QPlatformNativeInterface> mNativeInterface;
     QScopedPointer<QPlatformInputContext> mInputContext;
 #if QT_CONFIG(accessibility)
-    QScopedPointer<QPlatformAccessibility> mAccessibility;
+    mutable QScopedPointer<QPlatformAccessibility> mAccessibility;
 #endif
     bool mFailed = false;
     bool mClientBufferIntegrationInitialized = false;

commit a98c5f8ffb7ac2f04640b76772f1f9620e74401c

Fix typo
Correct a typo introduced with 0761173a.

Change-Id: I5c101e015c0fbd7de03d26ff3f8629418f070db2
Reviewed-by: David Edmundson <davidedmundson@kde.org>
Reviewed-by: Johan Helsing <johan.helsing@qt.io>

src/client/qwaylandintegration.cpp
@@ -272,7 +272,7 @@ QPlatformAccessibility *QWaylandIntegration::accessibility() const
 {
     if (!mAccessibility) {
 #ifndef QT_NO_ACCESSIBILITY_ATSPI_BRIDGE
-        Q_ASSERT_X(QCoreApplication::eventDispatcher(), "QXcbIntegration",
+        Q_ASSERT_X(QCoreApplication::eventDispatcher(), "QWaylandIntegration",
             "Initializing accessibility without event-dispatcher!");
         mAccessibility.reset(new QSpiAccessibleBridge());
 #else
112d4887a3b33b63826aad7b5d9dbe876c462146
Cristian Adam <cristian.adam@qt.io>
2020-11-19 19:49:46 +0100

    CMake: Fix Waylandkms and XComposite CMake find packages
    The packages were including FindPkgConfig.cmake directly instead
    of calling find_package(PkgConfig), which was causing these warnings.
    
    Fixes: QTBUG-88539
    Change-Id: Ib2947b26df1abdb5c45883f89dc51fbece0b9f20
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- cmake/FindWaylandkms.cmake
+++ cmake/FindWaylandkms.cmake
@@ -1,4 +1,9 @@
-include(FindPkgConfig)
+if (TARGET PkgConfig::Waylandkms)
+    set(Waylandkms_FOUND 1)
+    return()
+endif()
+
+find_package(PkgConfig QUIET)
 
 pkg_check_modules(Waylandkms wayland-kms IMPORTED_TARGET)
 
--- cmake/FindXComposite.cmake
+++ cmake/FindXComposite.cmake
@@ -1,4 +1,9 @@
-include(FindPkgConfig)
+if (TARGET PkgConfig::XComposite)
+    set(XComposite_FOUND 1)
+    return()
+endif()
+
+find_package(PkgConfig QUIET)
 
 pkg_check_modules(XComposite xcomposite IMPORTED_TARGET)
 

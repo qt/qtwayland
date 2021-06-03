256ba50d7bb184fa3e75225f70b295b194af9ace
Fabian Kosmale <fabian.kosmale@qt.io>
2021-06-03 12:38:16 +0200

    Add "We mean it" to private headers
    Change-Id: Ia5c21421be7bc546b04002932dd5571464152635
    Reviewed-by: Ulf Hermann <ulf.hermann@qt.io>

--- src/hardwareintegration/client/wayland-egl/qwaylandeglinclude_p.h
+++ src/hardwareintegration/client/wayland-egl/qwaylandeglinclude_p.h
@@ -37,6 +37,18 @@
 **
 ****************************************************************************/
 
+
+//
+//  W A R N I N G
+//  -------------
+//
+// This file is not part of the Qt API.  It exists purely as an
+// implementation detail.  This header file may change from version to
+// version without notice, or even be removed.
+//
+// We mean it.
+//
+
 #ifndef QWAYLANDEGLINCLUDE_H
 #define QWAYLANDEGLINCLUDE_H
 

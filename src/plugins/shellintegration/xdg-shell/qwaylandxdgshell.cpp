48bdfa057910f5f7f7e9938bb69584c8638e286b
Liang Qi <liang.qi@qt.io>
2022-01-04 12:30:36 +0100

    client: set_constraint_adjustment() for popups in xdg
    See also https://cgit.freedesktop.org/wayland/wayland-protocols/tree/stable/xdg-shell/xdg-shell.xml#n234 .
    
    Kudos to Greg V for his original patch in jira.
    
    Fixes: QTBUG-87303
    Done-with: Greg V <greg@unrelenting.technology>
    Change-Id: I57df9aedea7cc6f0b6fa142a6fc6c3bdc98324c8
    Reviewed-by: David Edmundson <davidedmundson@kde.org>
    (cherry picked from commit 59a5fe99e1569421b920d99c5b20cdafcdcf43a9)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugins/shellintegration/xdg-shell/qwaylandxdgshell.cpp
+++ src/plugins/shellintegration/xdg-shell/qwaylandxdgshell.cpp
@@ -435,6 +435,10 @@ void QWaylandXdgSurface::setPopup(QWaylandWindow *parent)
     positioner->set_anchor(QtWayland::xdg_positioner::anchor_top_left);
     positioner->set_gravity(QtWayland::xdg_positioner::gravity_bottom_right);
     positioner->set_size(m_window->geometry().width(), m_window->geometry().height());
+    positioner->set_constraint_adjustment(QtWayland::xdg_positioner::constraint_adjustment_slide_x
+        | QtWayland::xdg_positioner::constraint_adjustment_slide_y
+        | QtWayland::xdg_positioner::constraint_adjustment_flip_x
+        | QtWayland::xdg_positioner::constraint_adjustment_flip_y);
     m_popup = new Popup(this, parentXdgSurface, positioner);
     positioner->destroy();
     delete positioner;

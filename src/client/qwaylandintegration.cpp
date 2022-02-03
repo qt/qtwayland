8d2881f1f3e53cfae99e5d486fbba60d2b2a7d0e
Joni Poikelin <joni.poikelin@qt.io>
2022-02-03 14:01:50 +0200

    Fix crash if no input method module could be loaded
    Change-Id: I8f346def616606a6c5540856bd08a84ee7ed5ca2
    Reviewed-by: David Edmundson <davidedmundson@kde.org>
    (cherry picked from commit 49fb7248f6ab7de046e2179c7861951ea1169e9b)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/client/qwaylandintegration.cpp
+++ src/client/qwaylandintegration.cpp
@@ -534,7 +534,7 @@ void QWaylandIntegration::reconfigureInputContext()
     }
 #endif
 
-    qCDebug(lcQpaWayland) << "using input method:" << inputContext()->metaObject()->className();
+    qCDebug(lcQpaWayland) << "using input method:" << (inputContext() ? inputContext()->metaObject()->className() : "<none>");
 }
 
 QWaylandShellIntegration *QWaylandIntegration::createShellIntegration(const QString &integrationName)

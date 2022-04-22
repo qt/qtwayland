dd0c8a2f9c511a8db2ad342956acd7dba9314ccd
Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@qt.io>
2022-04-22 09:10:20 +0200

    doc: Add missing qml module landing pages
    When these are missing, external links to the contents of the modules
    break. Any \inqmlmodule should always have a corresponding \qmlmodule.
    
    Fixes: QTBUG-102379
    Change-Id: Id800e1ac3976957869f4a521f69f0190fb7eba5a
    Reviewed-by: Paul Olav Tvete <paul.tvete@qt.io>
    (cherry picked from commit aee5dbf01c24a1487847d996647b0c53f1f20e8b)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/imports/compositor-extensions/qtshell/qwaylandqtshellplugin.cpp
+++ src/imports/compositor-extensions/qtshell/qwaylandqtshellplugin.cpp
@@ -38,6 +38,29 @@ QT_BEGIN_NAMESPACE
 
 Q_COMPOSITOR_DECLARE_QUICK_EXTENSION_CLASS(QWaylandQtShell)
 
+/*!
+    \qmlmodule QtWayland.Compositor.QtShell
+    \title Qt Wayland Qt Shell Extension
+    \ingroup qmlmodules
+    \since 6.3
+    \brief Provides a shell extension for Qt applications running on a Qt Wayland Compositor.
+
+    \section2 Summary
+    The QtShell extension provides a way to associate an QtShellSurface with a regular Wayland
+    surface. The QtShell extension is written to support the window management features which are
+    supported by Qt. It may be suitable on a platform where both the compositor and client
+    applications are written with Qt, and where applications are trusted not to abuse features such
+    as manual window positioning and "bring-to-front".
+
+    For other use cases, consider using IviApplication or XdgShell instead.
+
+    \section2 Usage
+    To use this module, import it like this:
+    \qml
+    import QtWayland.Compositor.IviApplication
+    \endqml
+*/
+
 class QQtWaylandShellPlugin : public QQmlExtensionPlugin
 {
     Q_OBJECT
--- src/imports/compositor-extensions/wlshell/qwaylandcompositorwlshellplugin.cpp
+++ src/imports/compositor-extensions/wlshell/qwaylandcompositorwlshellplugin.cpp
@@ -37,6 +37,26 @@ QT_BEGIN_NAMESPACE
 
 Q_COMPOSITOR_DECLARE_QUICK_EXTENSION_CLASS(QWaylandWlShell)
 
+/*!
+    \qmlmodule QtWayland.Compositor.WlShell
+    \title Qt Wayland WlShell extension
+    \ingroup qmlmodules
+    \brief Provides a Qt API for the WlShell extension.
+
+    \section2 Summary
+    WlShell is a shell extension providing window system features typical to
+    desktop systems. It is superseded by XdgShell and exists in Qt mainly
+    for backwards compatibility with older applications.
+
+    WlShell corresponds to the Wayland interface \c wl_shell.
+
+    \section2 Usage
+    To use this module, import it like this:
+    \qml
+    import QtWayland.Compositor.WlShell
+    \endqml
+*/
+
 class QWaylandCompositorWlShellPlugin : public QQmlExtensionPlugin
 {
     Q_OBJECT

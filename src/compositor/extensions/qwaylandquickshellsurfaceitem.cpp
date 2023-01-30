qt/qtwayland

commit d89c8920f3b82dd2098971b5a66c4b9c75da5af0

Introduce new qt-shell and an API for custom shells
Adds a new API for writing custom shell extensions. This API is supported,
but semi-public. Binary compatibility is not guaranteed.

Also adds qt-shell, a new shell that maps directly to the QWindow API, and
provides functionality that Qt provides on other window systems, such as
absolute window positions and window activation. This shell is not intended
for use on the desktop.

This is a squashed commit of a development branch consisting of approximately
60 changes. Contributors:
  Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@qt.io>
  Paul Olav Tvete <paul.tvete@qt.io>

Task-number: QTBUG-94330
Task-number: QTBUG-91542
Change-Id: I419b6bd8179fe03e4da47d328c7ff4b4795b8a91
Reviewed-by: Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@qt.io>
Reviewed-by: David Edmundson <davidedmundson@kde.org>
Reviewed-by: Paul Olav Tvete <paul.tvete@qt.io>

src/compositor/extensions/qwaylandquickshellsurfaceitem.cpp
@@ -313,4 +313,127 @@ void QWaylandQuickShellEventFilter::timerEvent(QTimerEvent *event)
     }
 }
 
+static QWaylandQuickShellSurfaceItem *findSurfaceItemFromMoveItem(QQuickItem *moveItem)
+{
+    if (Q_UNLIKELY(!moveItem))
+        return nullptr;
+    if (auto *surf = qobject_cast<QWaylandQuickShellSurfaceItem *>(moveItem))
+        return surf;
+    for (auto *item : moveItem->childItems()) {
+        if (auto *surf = findSurfaceItemFromMoveItem(item))
+            return surf;
+    }
+    return nullptr;
+}
+
+/*
+    To raise a surface, find the topmost suitable surface and place above that.
+    We start from the top and:
+    If we don't have staysOnTop, skip all surfaces with staysOnTop
+    If we have staysOnBottom, skip all surfaces that don't have staysOnBottom
+  */
+void QWaylandQuickShellSurfaceItemPrivate::raise()
+{
+    Q_Q(QWaylandQuickShellSurfaceItem);
+    auto *moveItem = q->moveItem();
+    QQuickItem *parent = moveItem->parentItem();
+    if (!parent)
+        return;
+    auto it = parent->childItems().crbegin();
+    auto skip = [this](QQuickItem *item) {
+        if (auto *surf = findSurfaceItemFromMoveItem(item))
+            return (!staysOnTop && surf->staysOnTop()) || (staysOnBottom && !surf->staysOnBottom());
+        return true; // ignore any other Quick items that may be there
+    };
+    while (skip(*it))
+        ++it;
+    QQuickItem *top = *it;
+    if (moveItem != top)
+        moveItem->stackAfter(top);
+}
+
+/*
+    To lower a surface, find the lowest suitable surface and place below that.
+    We start from the bottom and:
+    If we don't have staysOnBottom, skip all surfaces with staysOnBottom
+    If we have staysOnTop, skip all surfaces that don't have staysOnTop
+  */
+void QWaylandQuickShellSurfaceItemPrivate::lower()
+{
+    Q_Q(QWaylandQuickShellSurfaceItem);
+    auto *moveItem = q->moveItem();
+    QQuickItem *parent = moveItem->parentItem();
+    if (!parent)
+        return;
+    auto it = parent->childItems().cbegin();
+
+    auto skip = [this](QQuickItem *item) {
+        if (auto *surf = findSurfaceItemFromMoveItem(item))
+            return (!staysOnBottom && surf->staysOnBottom()) || (staysOnTop && !surf->staysOnTop());
+        return true; // ignore any other Quick items that may be there
+    };
+    while (skip(*it))
+        ++it;
+
+    QQuickItem *bottom = *it;
+    if (moveItem != bottom)
+        moveItem->stackBefore(bottom);
+}
+
+/*!
+ * \property QWaylandQuickShellSurfaceItem::staysOnTop
+ *
+ * Keep this item above other Wayland surfaces
+ */
+bool QWaylandQuickShellSurfaceItem::staysOnTop() const
+{
+    Q_D(const QWaylandQuickShellSurfaceItem);
+    return d->staysOnTop;
+}
+
+void QWaylandQuickShellSurfaceItem::setStaysOnTop(bool onTop)
+{
+    Q_D(QWaylandQuickShellSurfaceItem);
+    if (d->staysOnTop == onTop)
+        return;
+    d->staysOnTop = onTop;
+    if (d->staysOnBottom) {
+        d->staysOnBottom = false;
+        emit staysOnBottomChanged();
+    }
+    // We need to call raise() even if onTop is false, since we need to stack under any other
+    // staysOnTop surfaces in that case
+    raise();
+    emit staysOnTopChanged();
+    Q_ASSERT(!(d->staysOnTop && d->staysOnBottom));
+}
+
+/*!
+ * \property QWaylandQuickShellSurfaceItem::staysOnBottom
+ *
+ * Keep this item above other Wayland surfaces
+ */
+bool QWaylandQuickShellSurfaceItem::staysOnBottom() const
+{
+    Q_D(const QWaylandQuickShellSurfaceItem);
+    return d->staysOnBottom;
+}
+
+void QWaylandQuickShellSurfaceItem::setStaysOnBottom(bool onBottom)
+{
+    Q_D(QWaylandQuickShellSurfaceItem);
+    if (d->staysOnBottom == onBottom)
+        return;
+    d->staysOnBottom = onBottom;
+    if (d->staysOnTop) {
+        d->staysOnTop = false;
+        emit staysOnTopChanged();
+    }
+    // We need to call lower() even if onBottom is false, since we need to stack over any other
+    // staysOnBottom surfaces in that case
+    lower();
+    emit staysOnBottomChanged();
+    Q_ASSERT(!(d->staysOnTop && d->staysOnBottom));
+}
+
 QT_END_NAMESPACE

commit 0088b09e79da01534a703f69dbccbd721e7594d8

compositor: Fix crash when raising shell surface item
This was introduced by d89c8920f3b82dd2098971b5a66c4b9c75da5af0.

The raise() function would search for a suitable surface, but
failing to find one, it would search past the end of the
childItems list and cause memory corruption.

[ChangeLog][QtWaylandCompositor] Fixed an issue where the compositor
would sometimes crash if a shell surface item was brought to front.

Pick-to: 6.4 6.5
Fixes: QTBUG-109051
Change-Id: I2249f0881b90fc05b5f0292cd35c6524db4663c5
Reviewed-by: Paul Olav Tvete <paul.tvete@qt.io>

src/compositor/extensions/qwaylandquickshellsurfaceitem.cpp
@@ -319,11 +319,14 @@ void QWaylandQuickShellSurfaceItemPrivate::raise()
             return (!staysOnTop && surf->staysOnTop()) || (staysOnBottom && !surf->staysOnBottom());
         return true; // ignore any other Quick items that may be there
     };
-    while (skip(*it))
+    auto end = parent->childItems().crend();
+    while (it != end && skip(*it))
         ++it;
-    QQuickItem *top = *it;
-    if (moveItem != top)
-        moveItem->stackAfter(top);
+    if (it != end) {
+        QQuickItem *top = *it;
+        if (moveItem != top)
+            moveItem->stackAfter(top);
+    }
 }
 
 /*
a3ac80da3e843f824831ad143d2f215eeffce139
Paul Olav Tvete <paul.tvete@qt.io>
2023-11-10 15:47:03 +0100

    Make custom-extension example work on Nvidia EGLStreams
    Fixes: QTBUG-111022
    Pick-to: 6.6 6.5
    Change-Id: I766eeb83dc019acab7fe0c8d29412ea28b8fbc56
    Reviewed-by: Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@qt.io>
    Reviewed-by: Ari Parkkila <ari.parkkila@qt.io>

--- examples/wayland/custom-extension/compositor/main.cpp
+++ examples/wayland/custom-extension/compositor/main.cpp
@@ -13,6 +13,9 @@
 
 int main(int argc, char *argv[])
 {
+    // ShareOpenGLContexts is needed for using the threaded renderer
+    // on Nvidia EGLStreams
+    QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts, true);
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine appEngine(QUrl("qrc:///qml/main.qml"));
 

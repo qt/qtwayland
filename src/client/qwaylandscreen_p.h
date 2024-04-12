f7a2d52336123d30253cc3548d4a971167b3d965
Liang Qi <liang.qi@qt.io>
2024-04-12 13:29:56 +0200

    client: implement QWaylandScreen::topLevelAt()
    For security reason, Wayland doesn't provide global position for
    top level windows in most cases.
    
    Task-number: QTBUG-113404
    Pick-to: 6.7 6.5 6.2 5.15
    Change-Id: I2cd11b641fba6582cf96cfbea16f5e598a473db5
    Reviewed-by: David Edmundson <davidedmundson@kde.org>

--- src/client/qwaylandscreen_p.h
+++ src/client/qwaylandscreen_p.h
@@ -63,6 +63,8 @@ public:
     QDpi logicalDpi() const override;
     QList<QPlatformScreen *> virtualSiblings() const override;
 
+    QWindow *topLevelAt(const QPoint &point) const override;
+
     Qt::ScreenOrientation orientation() const override;
     int scale() const;
     qreal devicePixelRatio() const override;
